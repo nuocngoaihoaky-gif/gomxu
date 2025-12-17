75896a91bcf05ce5477c5cfb4e1085525c5eea1c
nuocngoaihoaky-gif <nuocngoaihoaky@gmail.com>
2025-12-18 02:19:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import os
 import requests
 KEY = os.environ.get("INIT_DATA")
-HEAD = {
+HEADERS = {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
     "content-type": "application/json",
     "origin": "https://gomxu.online",
-    "user-agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+    "referrer": "https://gomxu.online/",
+    "sec-ch-ua": '"Chromium";v="137", "Not/A)Brand";v="24"',
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": '"Android"',
+    "user-agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36"
 }
 def run():
     if not KEY: return
-    data = {"initData": KEY}
-    try: requests.post("https://gomxu.site/mining", headers=HEAD, json=data, timeout=5)
+    payload = {"initData": KEY}
+    try:
+        requests.post("https://gomxu.site/mining", headers=HEADERS, json=payload, timeout=5)
     except: pass
-    try: requests.post("https://gomxu.site/viewads", headers=HEAD, json={**data, "typeReward": "gold"}, timeout=5)
+    try:
+        data_ads = {**payload, "typeReward": "goldCoin"}
+        requests.post("https://gomxu.site/viewads", headers=HEADERS, json=data_ads, timeout=5)
     except: pass
 if __name__ == "__main__":
     run()
